98731dc891a33b43f812ff8d100bd274abfcf0d1
Diego Gonzalez Morin <diegogm@kth.se>
2017-05-03 11:28:28 +0200

    Train 1

---
+++ Lab 3/Report.m
@@ -0,0 +1,9 @@
+%% DT2119 Lab3B: Deep Learning for ASR
+%
+%% 4.A. Training and testing
+%
+%% 4.A.1 Prepare the data base
+%
+% The objectivo is to not only prepare the data base, but understand them. 
+% We include the necessary modules:
+system('source tools/modules');
